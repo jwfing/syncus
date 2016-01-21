d0bef80636f13449de564df9d568adbeefccf999
junwen <jwfing@gmail.com>
2016-01-22 00:18:45 +0800

    modify todo appearence

--- routes/todos.js
+++ routes/todos.js
@@ -8,11 +8,11 @@ var Todo = AV.Object.extend('Todo');
 // 查询 Todo 列表
 router.get('/', function(req, res, next) {
   var query = new AV.Query(Todo);
-  query.descending('createdAt');
+  query.ascending('createdAt');
   query.find({
     success: function(results) {
       res.render('todos', {
-        title: 'TODO 列表',
+        title: 'jwfing\'s TODO',
         todos: results
       });
     },
@@ -21,7 +21,7 @@ router.get('/', function(req, res, next) {
         // 该错误的信息为：{ code: 101, message: 'Class or object doesn\'t exists.' }，说明 Todo 数据表还未创建，所以返回空的 Todo 列表。
         // 具体的错误代码详见：https://leancloud.cn/docs/error_code.html
         res.render('todos', {
-          title: 'TODO 列表',
+          title: 'jwfing\'s TODO',
           todos: []
         });
       } else {
